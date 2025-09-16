143283a3b287a5a5376ea14f1ae465f00fe28fa7
Vinothkumar <vinoth3941@gmail.com>
2025-09-16 10:48:45 +0530

    Updated application name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-standalone-app' title`, () => {
+  it(`should have the 'azhal-website' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-standalone-app');
+    expect(app.title).toEqual('azhal-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-standalone-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, azhal-website');
   });
 });
