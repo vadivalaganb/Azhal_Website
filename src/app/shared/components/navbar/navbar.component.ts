6a1de8f25c67e77f97be416fcdec78a0a3b4e23a
Vinothkumar <vinoth3941@gmail.com>
2025-11-18 14:41:36 +0530

    Navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -11,12 +11,36 @@ import { RouterLink, RouterModule } from "@angular/router";
 })
 export class NavbarComponent {
   isSticky = false;
-  showBackToTop = false;
+  isMobile = false;
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768; // mobile breakpoint
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isSticky = scrollTop > 45;
-    this.showBackToTop = scrollTop > 100;
+
+    // Sticky only for desktop
+    if (!this.isMobile) {
+      this.isSticky = scrollTop > 45;
+    } else {
+      this.isSticky = true; // always sticky for mobile
+    }
   }
+
+  // isSticky = false;
+  // showBackToTop = false;
+
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   const scrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  //   this.isSticky = scrollTop > 45;
+  //   this.showBackToTop = scrollTop > 100;
+  // }
 }
